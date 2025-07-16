577fb7c123e424ddf9e5e799af8b89a60fdf258e
Leperlier Nathaniel <nathaniel.leperlier@epitech.eu>
2025-07-16 10:47:13 +0400

    try hidden keyboards v.3

--- frontend/src/utils/focus_scan.ts
+++ frontend/src/utils/focus_scan.ts
@@ -1,6 +1,9 @@
 export function focusScannerInput() {
   const input = document.getElementById("for_scan");
   if (input) {
+    console.log("element trouve")
     input.focus();
+  } else {
+    console.log("element non trouver")
   }
 }
